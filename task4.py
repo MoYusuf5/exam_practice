358d24e5d04cbb41d83b9b4fca1f5f02a56e7ba6
Mohamed <mohamedyusuf45@hotmail.com>
2022-10-17 10:25:50 +0100

    first commit

--- task4.py
+++ task4.py
@@ -3,7 +3,7 @@ password = input("please enter your password")
 def length(password):
     if len(password) < 5:
         print("Your Password is Too Short")
-    elif len(password) > 5 and len(password) < 21:
+    elif len(password) >= 5 and len(password) <= 20:
         print("Your Password is an acceptable length")
     elif len(password) > 20:
         print("your password is too long and may be forgotten")
